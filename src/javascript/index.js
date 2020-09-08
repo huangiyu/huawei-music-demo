75e9045b7d9c5cf00055c4b8840901b22b95e1c0
huangiyu <huangyu9673@gmail.com>
2020-09-08 10:49:47 +0800

    播放以及切歌按钮

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,68 @@
-console.log(2);
+import "./icons.js";
+
+const $ = (selector) => document.querySelector(selector);
+const $$ = (selector) => document.querySelectorAll(selector);
+
+class Player {
+  constructor(node) {
+    this.root = typeof node === "string" ? $(node) : node;
+    this.songList = [];
+    this.currentIndex = 0;
+    this.audio = new Audio();
+    this.start();
+    this.bind();
+    //https://huangiyu.github.io/data-mock/huawei-music/music-list.json
+  }
+
+  start() {
+    fetch("https://huangiyu.github.io/data-mock/huawei-music/music-list.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        this.songList = data;
+        this.audio.src = this.songList[this.currentIndex].url;
+      });
+  }
+
+  bind() {
+    let self = this;
+    this.root.querySelector(".btn-play-pause").onclick = function () {
+      if (this.classList.contains("playing")) {
+        self.audio.pause();
+        this.classList.remove("playing");
+        this.classList.add("pause");
+        this.querySelector("use").setAttribute("xlink:href", "#icon-play");
+      } else if (this.classList.contains("pause")) {
+        self.audio.play();
+        this.classList.remove("pause");
+        this.classList.add("playing");
+        this.querySelector("use").setAttribute("xlink:href", "#icon-pause");
+      }
+    };
+    this.root.querySelector(".btn-pre").onclick = function () {
+      self.playPrevsong();
+    };
+    this.root.querySelector(".btn-next").onclick = function () {
+      self.playNextsong();
+    };
+  }
+
+  playPrevsong() {
+    this.currentIndex =
+      (this.songList.length + this.currentIndex - 1) % this.songList.length;
+    this.audio.src = this.songList[this.currentIndex].url;
+    this.audio.oncanplaythrough = () => {
+      this.audio.play();
+    };
+  }
+  playNextsong() {
+    this.currentIndex =
+      (this.songList.length + this.currentIndex + 1) % this.songList.length;
+    this.audio.src = this.songList[this.currentIndex].url;
+    this.audio.oncanplaythrough = () => {
+      this.audio.play();
+    };
+  }
+}
+
+new Player("#player");
